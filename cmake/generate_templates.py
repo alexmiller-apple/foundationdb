fe98e930f1a5efba170d069d50d9ae4c2617b903
Alex Miller <alexmiller@apple.com>
2019-07-28 19:59:32 -0700

    Port to makefiles
    * Docker image only has python 2.6
    * It's far easier to just generate empty files for projects that don't
      have templates to instantiate.
    * Generating multiple files from one command is hard in GNU Make

--- cmake/generate_templates.py
+++ cmake/generate_templates.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import json
-import argparse
+import optparse
 import filecmp
 import os
 import shutil
@@ -11,34 +11,31 @@ class InternalError(Exception):
     pass
 
 
-class TemplateGenerator:
+class TemplateGenerator(object):
     def __init__(self, args):
         self.targetName = args.target
         self.numFiles = args.num_files
         self.fileName = args.out
         with open(args.file, 'r') as f:
             self.descr = json.load(f)
-        if args.target not in self.descr:
-            print('ERROR: undefined target {}'.format(args.target))
-            raise InternalError
 
     def generate(self):
-        target = self.descr[self.targetName]
+        target = self.descr.get(self.targetName, {})
         dependencies = []
         for dep in target['dependencies'] if 'dependencies' in target else []:
             if dep not in self.descr:
-                print("ERROR: {} was declared as dependency of {} but does not exist".format(dep, self.targetName))
+                print("ERROR: %s was declared as dependency of %s but does not exist"%(dep, self.targetName))
                 raise InternalError
             dependencies.append((dep, self.descr[dep]))
         for i in range(0, self.numFiles):
-            outFile = "{}{}.cpp".format(self.fileName, i)
-            tmpFile = "{}.tmp".format(outFile)
+            outFile = "%s%d.cpp"%(self.fileName, i)
+            tmpFile = "%s.tmp"%(outFile,)
             with open(tmpFile, 'w') as f:
                 f.write('// This file was generated - DO NOT CHANGE\n\n')
                 for include in target["includes"] if 'includes' in target else []:
-                    f.write('#include "{}"\n'.format(include))
+                    f.write('#include "%s"\n'%(include,))
                 for include in target["sysincludes"] if 'sysincludes' in target else []:
-                    f.write('#include <{}>\n'.format(include))
+                    f.write('#include <%s>\n'%(include,))
                 j = 0
                 f.write('\n\n\n')
                 templates = target['templates'] if 'templates' in target else []
@@ -47,10 +44,10 @@ class TemplateGenerator:
                     for dep in dependencies:
                         ts = dep[1]['templates'] if 'templates' in dep[1] else []
                         if template in ts:
-                            print("ERROR: {} is also defined in {} which is a dependency".format(template, dep[0]))
+                            print("ERROR: %s is also defined in %s which is a dependency"%(template, dep[0]))
                             raise InternalError
                     if j % self.numFiles == i:
-                        f.write('template struct {};\n'.format(template))
+                        f.write('template struct %s;\n'%(template,))
                     j += 1
             # Compiling these files will be quite expensive. So we make sure to only
             # write them if they have changed
@@ -58,11 +55,11 @@ class TemplateGenerator:
                 shutil.copyfile(tmpFile, outFile)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Generate explicit template specification")
-    parser.add_argument('file', type=str, help='The json file with the definitions')
-    parser.add_argument('-N', '--num-files', type=int, default=8)
-    parser.add_argument('-o', '--out', type=str, default="SerializeImpl")
-    parser.add_argument('-t', '--target', type=str, required=True)
-    args = parser.parse_args()
-    generator = TemplateGenerator(args)
+    parser = optparse.OptionParser(description="Generate explicit template specification")
+    parser.add_option('-N', '--num-files', type=int, default=8)
+    parser.add_option('-o', '--out', type=str, default="SerializeImpl")
+    parser.add_option('-t', '--target', type=str)
+    options, args = parser.parse_args()
+    options.file = args[0]
+    generator = TemplateGenerator(options)
     generator.generate()
